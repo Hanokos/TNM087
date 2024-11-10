3016d6c4ddcb3c5ad8621e76b35cc8b7fe0bbf32
Hanokos <119046964+Hanokos@users.noreply.github.com>
2024-11-10 16:28:04 +0100

    Add files via upload
    TO test different Oimage

---
+++ Task2_testcode.m
@@ -0,0 +1,15 @@
+% Read the image 'spillway-dark.tif'
+
+%OImage = imread('aerialview-washedout.tif');
+%OImage = imread('spillway-dark.tif');
+OImage = imread('IntensityRampGamma25.tif');
+
+
+% Apply gamma correction with 
+Gamma = 0.4;
+Lower = 0.0;
+Upper = 1.0;
+GImage = GammaCorrection(OImage, Gamma, Lower, Upper);
+
+% Display the image
+imshow(GImage);
